f5ffc1663fb1d1b099972fc96b222cbf1a7ac8b7
ivanloav <ivanloav@gmail.com>
2023-11-19 10:06:11 +0100

    Fichero modificado

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -1,4 +1,12 @@
-console.log("hello world");
+// aboutme.js
 
+// Variables que almacenan información personal
+let nombreCompleto = "Iván López";
+let profesion = "Administración de sistemas";
+let puestoDeseado = "Full Stack Web development";
 
-console.log("laater");
+// Variable que utiliza template literals
+let mensajePersonal = `Hola, me llamo ${nombreCompleto} y me dedico a ${profesion}. Estoy cursando este Máster porque me gustaría trabajar en ${puestoDeseado}`;
+
+// Mostrar el mensaje por consola
+console.log(mensajePersonal);
